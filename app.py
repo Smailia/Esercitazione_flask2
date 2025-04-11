15963e3211bfc6ec517bed2cbacdee06c4c6b424
Smailia <smailia.omar@iisgalvanimi.edu.it>
2025-04-11 10:59:00 +0000

    2

--- app.py
+++ app.py
@@ -1,7 +1,14 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
+lista =  1,2,3,3,4,4,4,5,5,6,6,6,6
+
 @app.route("/")
 def home():
-    return "per ora funziona tutto"
+    return render_template('index.html', lista = lista)
+@app.route("/profilo")
+def profilo():
+   return "questa è la pagina profilo"
+
+
 if __name__ == '__main__':
     app.run(debug=True)
